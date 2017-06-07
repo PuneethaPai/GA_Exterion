b6b7c586faf90ae1ff24d779289cd987547d2852
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-06-07 17:59:35 +0530

    Bucket: add this class to form bucket to hold scoreCards

---
+++ src/bucket.py
@@ -0,0 +1,12 @@
+class Bucket(object):
+    def __init__(self, score_cards):
+        self.score_cards = set(score_cards)
+
+    def __equal__(self, other):
+        return self.score_cards == other.score_cards
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __str__(self):
+        return reduce(lambda x, y: str(x) + str(y), self.score_cards)
