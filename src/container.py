0b8b5e0660f831a0cec0f6aa50ca85aa18afec72
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-06-08 23:10:28 +0530

    Container: Add equals and hashcode implementation

--- src/container.py
+++ src/container.py
@@ -1,14 +1,24 @@
 # Understands the solution module ie to create buckets of equal size as to holds score card in them
 from itertools import islice
 
+from bucket import Bucket
+
 
 class Container(object):
     def __init__(self, dna, number_of_buckets):
         self.dna = dna
         self.buckets = self.__generate_buckets(dna, number_of_buckets)
 
+    def __eq__(self, other):
+        return self.dna == other.dna and self.buckets == other.buckets
+
+    def __hash__(self):
+        return hash(frozenset(self.buckets))
+
     def __generate_buckets(self, dna, number_of_buckets):
         score_cards = dna.structure
         bucket_size = len(score_cards) / number_of_buckets
         iterable = iter(score_cards)
-        return list(iter(lambda: tuple(islice(iterable, bucket_size)), ()))
+        buckets = list(iter(lambda: tuple(islice(iterable, bucket_size)), ()))
+        buckets = map(lambda x: Bucket(x), buckets)
+        return buckets
--- test/container_test.py
+++ test/container_test.py
@@ -11,6 +11,24 @@ class ContainerTest(unittest.TestCase):
         container = Container(dna=dna, number_of_buckets=5)
         self.assertEquals(5, len(container.buckets))
 
+    def test_container_equality(self):
+        dna1 = self.get_a_dna_with_some_structure()
+        dna2 = self.get_a_dna_with_some_structure()
+        self.assertNotEqual(dna1, dna2)
+        container1 = Container(dna1, 5)
+        container2 = Container(dna2, 5)
+        self.assertEquals(container1, container2)
+        self.assertEquals(container1.__hash__(), container2.__hash__())
+
+    def test_container_inequality(self):
+        dna1 = DNA([ScoreCard(1, 10), ScoreCard(2, 20)])
+        dna2 = DNA([ScoreCard(3, 30), ScoreCard(4, 40)])
+        self.assertNotEqual(dna1, dna2)
+        container1 = Container(dna1, 1)
+        container2 = Container(dna2, 1)
+        self.assertNotEqual(container1, container2)
+        self.assertNotEqual(container1.__hash__(), container2.__hash__())
+
     def get_a_dna_with_some_structure(self):
         cards = [ScoreCard(1, 10), ScoreCard(2, 20)] * 5
         return DNA(structure=cards)
