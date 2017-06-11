7e50beaaeb7b5c65be09b83b1fbc7751a12038fd
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-06-11 21:10:29 +0530

    DNA: Add feature for dna to crossover and produce 2 children

--- src/dna.py
+++ src/dna.py
@@ -1,4 +1,5 @@
 # Understands score card arrangement i.e structure
+import random
 
 
 class DNA(object):
@@ -13,3 +14,16 @@ class DNA(object):
 
     def __str__(self):
         return reduce(lambda x, y: str(x) + str(y), self.structure)
+
+    def crossover(self, other):
+        crossover_point = random.randint(0, len(self.structure))
+        child_1 = DNA(structure=self.structure[:crossover_point])
+        child_2 = DNA(structure=other.structure[:crossover_point])
+        child_1.__regenerate(other)
+        child_2.__regenerate(self)
+        return child_1, child_2
+
+    def __regenerate(self, other):
+        for score_card in other.structure:
+            if score_card not in self.structure:
+                self.structure.append(score_card)
--- test/dna_test.py
+++ test/dna_test.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 
 from dna import DNA
@@ -8,16 +9,36 @@ class DNA_Test(unittest.TestCase):
     def test_dna_with_same_structure_are_equal(self):
         dna1 = DNA(structure=[ScoreCard(1, 10), ScoreCard(2, 20)])
         dna2 = DNA(structure=[ScoreCard(1, 10), ScoreCard(2, 20)])
-        self.assertEquals(dna1,dna2)
+        self.assertEquals(dna1, dna2)
         self.assertEquals(dna1.__hash__(), dna2.__hash__())
 
     def test_dna_with_different_structure_are_not_equal(self):
         dna1 = DNA(structure=[ScoreCard(1, 10), ScoreCard(2, 20)])
         dna2 = DNA(structure=[ScoreCard(2, 20), ScoreCard(1, 10)])
-        self.assertNotEqual(dna1,dna2)
+        self.assertNotEqual(dna1, dna2)
         self.assertNotEqual(dna1.__hash__(), dna2.__hash__())
 
     def test_DNA_to_give_beautiful_object_response(self):
         dna1 = DNA(structure=[ScoreCard(1, 10), ScoreCard(2, 20)])
         dna2 = DNA(structure=[ScoreCard(1, 10), ScoreCard(2, 20)])
-        self.assertEquals(dna1.__str__(),dna2.__str__())
+        self.assertEquals(dna1.__str__(), dna2.__str__())
+
+    def test_dna_crossover_with_itself_gives_same_two_children(self):
+        dna = self.get_a_dna_with_some_structure()
+        child_1, child_2 = dna.crossover(dna)
+        self.assertEquals(child_1, child_2)
+
+    def test_dna_crossover_to_get_two_children(self):
+        dna1 = self.get_a_dna_with_some_structure()
+        dna2 = self.get_a_dna_with_some_structure()
+        random.shuffle(dna2.structure)
+        child_1, child_2 = dna1.crossover(dna2)
+        self.assertNotEqual(child_1, child_2)
+
+    def get_a_dna_with_some_structure(self):
+        cards = [ScoreCard(1, 10), ScoreCard(2, 20),
+                 ScoreCard(3, 30), ScoreCard(4, 40),
+                 ScoreCard(5, 50), ScoreCard(6, 60),
+                 ScoreCard(7, 70), ScoreCard(8, 80),
+                 ScoreCard(9, 90), ScoreCard(10, 100)]
+        return DNA(structure=cards)
