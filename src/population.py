94d30f3de1c87f84af597e4745a68f4b3247f259
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-06-12 11:11:47 +0530

    Population: Add functionality to sort containers based on the value of the fitness

--- src/population.py
+++ src/population.py
@@ -1,7 +1,19 @@
+# Understands a list of possible solutions and to handle creation of next population
+
+
 class Population(object):
+    kill_percentage = 0
+
     def __init__(self, containers):
         self.containers = containers
         self.fitness = self.__calculate_fitness(containers)
+        self.sort_containers_based_on_fitness()
 
     def __calculate_fitness(self, containers):
         return map(lambda x: x.fitness(), containers)
+
+    def sort_containers_based_on_fitness(self):
+        sorted_tuples = sorted(zip(self.fitness, self.containers))
+        sorted_tuples = sorted_tuples[::-1]
+        self.fitness = map(lambda x: x[0], sorted_tuples)
+        self.containers = map(lambda x: x[1], sorted_tuples)
--- test/population_test.py
+++ test/population_test.py
@@ -7,11 +7,11 @@ from population import Population
 
 
 class PopulationTest(unittest.TestCase):
-    def test_population_for_calculate_fitness(self):
+    def test_population_for_calculate_fitness_and_sort_containers(self):
         mock_container_1 = Mock(spec=Container)
         mock_container_2 = Mock(spec=Container)
         container_list = [mock_container_1, mock_container_2]
         mock_container_1.fitness = Mock(return_value=1)
         mock_container_2.fitness = Mock(return_value=2)
         population = Population(containers=container_list)
-        self.assertEquals(population.fitness, [1, 2])
+        self.assertEquals(population.fitness, [2, 1])
