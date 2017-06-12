34297def3a6e6836a04285ad4881c94d5db54976
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-06-12 15:36:31 +0530

    Nucleotide: Feature, create n dna with random combination of ScoreCards

--- src/nucleotide.py
+++ src/nucleotide.py
@@ -1,3 +1,8 @@
+import random
+
+from dna import DNA
+
+
 class Nucleotide(object):
     def __init__(self, score_cards):
         self.score_cards = set(score_cards)
@@ -7,3 +12,9 @@ class Nucleotide(object):
 
     def __hash__(self):
         return hash(frozenset(self.score_cards))
+
+    def create(self, size):
+        return map(lambda x: self.generate_a_dna(), xrange(size))
+
+    def generate_a_dna(self):
+        return DNA(random.sample(self.score_cards, len(self.score_cards)))
--- test/nucleotide_test.py
+++ test/nucleotide_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+from helper import get_a_dna_with_some_structure
 from nucleotide import Nucleotide
 from score_card import ScoreCard
 
@@ -16,3 +17,9 @@ class NucleotideTest(unittest.TestCase):
         nucleotide_2 = Nucleotide(score_cards=[ScoreCard(3, 30), ScoreCard(4, 40)])
         self.assertNotEqual(nucleotide_1, nucleotide_2)
         self.assertNotEqual(nucleotide_1.__hash__(), nucleotide_2.__hash__())
+
+    def test_nucleotide_create_n_random_dna(self):
+        dna = get_a_dna_with_some_structure()
+        nucleotide = Nucleotide(score_cards=dna.structure)
+        dnas = nucleotide.create(size=10)
+        self.assertEquals(len(dnas), 10)
