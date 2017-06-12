5886ff0e0af9a1104385d1b1ba44535a8d8716e5
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-06-12 12:19:13 +0530

    Population: Feature, generation of next_population using kill_percentage completed

--- src/population.py
+++ src/population.py
@@ -1,4 +1,5 @@
 # Understands a list of possible solutions and to handle creation of next population
+import random
 
 
 class Population(object):
@@ -17,3 +18,23 @@ class Population(object):
         sorted_tuples = sorted_tuples[::-1]
         self.fitness = map(lambda x: x[0], sorted_tuples)
         self.containers = map(lambda x: x[1], sorted_tuples)
+
+    def next_population(self):
+        kill_index = len(self.containers) * self.kill_percentage / 100
+        survivor_index = len(self.containers) - kill_index
+        survivors = self.containers[:survivor_index]
+        new_containers = self.__generate_next_descendents_from(survivors)
+        return Population(containers=new_containers)
+
+    def __generate_next_descendents_from(self, survivors):
+        new_containers = survivors[:]
+        while len(new_containers) < len(self.containers):
+            parent_1, parent_2 = self.__get_random_parent(survivors)
+            child_1, child_2 = parent_1.crossover(parent_2)
+            new_containers.append(child_1)
+            new_containers.append(child_2)
+        return new_containers
+
+    def __get_random_parent(self, survivors):
+        parents = random.sample(survivors, 2)
+        return parents
--- test/population_test.py
+++ test/population_test.py
@@ -8,10 +8,32 @@ from population import Population
 
 class PopulationTest(unittest.TestCase):
     def test_population_for_calculate_fitness_and_sort_containers(self):
+        containers = self.get_list_of_mock_containers()
+        population = Population(containers=containers)
+        self.assertEquals(population.fitness, sorted([2, 1] * 100)[::-1])
+
+    def test_population_generate_next_population_gives_same_population_for_zero_kill_percentage(self):
+        Population.kill_percentage = 0
+        containers = self.get_list_of_mock_containers()
+        population = Population(containers=containers)
+        next_population = population.next_population()
+        self.assertEquals(next_population.fitness, population.fitness)
+
+    def test_population_generate_next_population_gives_new_population_when_kill_percentage_is_hundred(self):
+        Population.kill_percentage = 50
+        containers = self.get_list_of_mock_containers()
+        population = Population(containers=containers)
+        next_population = population.next_population()
+        self.assertNotEqual(next_population.fitness, population.fitness)
+        self.assertEquals(len(next_population.containers), len(population.containers))
+
+    def get_list_of_mock_containers(self):
         mock_container_1 = Mock(spec=Container)
         mock_container_2 = Mock(spec=Container)
-        container_list = [mock_container_1, mock_container_2]
+        child_container_1 = Mock(spec=Container)
+        child_container_2 = Mock(spec=Container)
         mock_container_1.fitness = Mock(return_value=1)
         mock_container_2.fitness = Mock(return_value=2)
-        population = Population(containers=container_list)
-        self.assertEquals(population.fitness, [2, 1])
+        mock_container_1.crossover = mock_container_2.crossover = Mock(return_value=(child_container_1, child_container_2))
+        container_list = [mock_container_1, mock_container_2] * 100
+        return container_list
