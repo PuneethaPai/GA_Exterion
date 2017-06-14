7683e80ca682aa664615ec65e43946017785a3e2
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-06-14 17:43:59 +0530

    Configure: Modified the json structure to also include algorithm specific parameter

--- src/configure.py
+++ src/configure.py
@@ -14,8 +14,20 @@ class Configure(object):
     def __read(self, json_file):
         path = os.path.join(ASSETS, json_file)
         with open(path, 'rb') as file:
-            data = json.load(file)
+            json_data = json.load(file)
+            data = self.__dictionarify(json_data)
             return data
+
+    def __dictionarify(self, json_data):
+        data = {}
+        problem_data = json_data['data']
+        setup_data = json_data['setup']
+        data['score_cards'] = problem_data['score_cards']
+        data['number_of_buckets'] = problem_data['number_of_buckets']
+        data['population_size'] = setup_data['population_size']
+        data['kill_percentage'] = setup_data['kill_percentage']
+        data['mutation_percentage'] = setup_data['mutation_percentage']
+        return data
 
     def __form_nucleotide(self):
         cards = self.data['score_cards']
