a3d690742254fe889a0b6245fdef4ba2134c9a58
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-06-14 12:44:46 +0530

    Helper: Modified the start index of ScoreCard from 1 to 0

--- test/helper.py
+++ test/helper.py
@@ -3,9 +3,9 @@ from score_card import ScoreCard
 
 
 def get_a_dna_with_some_structure():
-    cards = [ScoreCard(1, 10), ScoreCard(2, 20),
-             ScoreCard(3, 30), ScoreCard(4, 40),
-             ScoreCard(5, 50), ScoreCard(6, 60),
-             ScoreCard(7, 70), ScoreCard(8, 80),
-             ScoreCard(9, 90), ScoreCard(10, 100)]
+    cards = [ScoreCard(0, 10), ScoreCard(1, 20),
+             ScoreCard(2, 30), ScoreCard(3, 40),
+             ScoreCard(4, 50), ScoreCard(5, 60),
+             ScoreCard(6, 70), ScoreCard(7, 80),
+             ScoreCard(8, 90), ScoreCard(9, 100)]
     return DNA(structure=cards)
